e26213709cd1a66c4b6832e1699ba2450eb07fa4
reddcs <chakirm082@gmail.com>
2026-03-25 12:52:05 +0100

    chore: update backend/database/factories/ProductFactory.php

---
+++ backend/database/factories/ProductFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProductFactory extends Factory
+{
+    protected $model = Product::class;
+
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->words(3, true),
+            'description' => $this->faker->paragraph(),
+            'price' => $this->faker->randomFloat(2, 10, 500),
+            'stock' => $this->faker->numberBetween(0, 100),
+            'category_id' => Category::factory(),
+            'image_path' => null,
+        ];
+    }
+}
